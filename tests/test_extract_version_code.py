7c69868faac04944a183ffdee7e3a5c5497dfb81
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-02 10:15:17 +0200

    Added extract_version_code method

---
+++ tests/test_extract_version_code.py
@@ -0,0 +1,4 @@
+from validate_version_code import extract_version_code, validate_version_code
+
+def test_extract_version_code():
+    return validate_version_code(extract_version_code("validate_version_code"))
--- validate_version_code/validate_version_code.py
+++ validate_version_code/validate_version_code.py
@@ -5,3 +5,8 @@ def validate_version_code(version_code:str)->bool:
         version_code:str, the version code to validate.
     """
     return bool(re.compile(r"\d+\.\d+\.\d+").match(version_code))
+
+def extract_version_code(path:str)->str:
+    """Return the version code from given project."""
+    with open("{path}/__version__.py".format(path=path), "r") as f:
+        return re.compile(r"(\d+\.\d+\.\d+)").findall(f.read())[0]
